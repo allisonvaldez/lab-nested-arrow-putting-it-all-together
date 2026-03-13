77ea6c6c67ae996cf71c90cd603ca6c4cdd11a12
allisonvaldez <alleyvaldez1@outlook.com>
2026-03-13 02:48:26 -0700

    done

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ function createLoginTracker(userInfo){
   // define and return anonymous nested arrow function with to handle login attempts
   return (passwordAttempt) => {
     attemptCount ++;
+
     // automatically check the password attempt count to trigger if >3 but not as a log with count number
     if (attemptCount > 3){
       return "Account locked due to too many failed login attempts";
-    } else if (passwordAttempt === userInfo.password && attemptCount <= 3){
-      return "Login successful.";
-    } else if (passwordAttempt != userInfo.password && attemptCount <= 3){
-      return "Login failed.";
-    } else {
-      return `Attempt ${attemptCount}: Login failed`
     }
+
+    if (passwordAttempt === userInfo.password && attemptCount <= 3){
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    } 
   };
 };
 
